2a9ff3bee16f3e936a0854fa3a5c37edfad8b430
Berzan <berzan@tutanota.com>
2022-10-22 18:41:11 +0300

    create: BaseChain.ts

---
+++ sdk/src/cosmos/BaseChain.ts
@@ -0,0 +1,18 @@
+import { ChainInfo, ChainURLs } from "./types/globals"
+
+export class BaseChain {
+    constructor(info: ChainInfo) {
+        this.name = info.name
+
+        this.urls = info.urls
+    }
+
+    readonly name: string
+
+    readonly urls: ChainURLs
+
+
+    readonly request = {
+
+    }
+}
