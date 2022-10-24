e7b0292e4b57559ea2e8122a40528948205177b1
Berzan <berzan@tutanota.com>
2022-10-24 13:50:21 +0300

    rename: Evmos.ts to EvmosChain.ts

--- sdk/src/cosmos/chains/EvmosChain.ts
+++ sdk/src/cosmos/chains/EvmosChain.ts
@@ -1,7 +1,7 @@
 import { BaseChain } from './BaseChain'
 import { EvmosInfo } from '../chain-infos'
 
-class Evmos extends BaseChain {
+export class EvmosChain extends BaseChain {
     constructor() {
         super(EvmosInfo)
     }
