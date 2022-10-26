017fd627dbb955ec35d1e0991f15cac375984f02
Berzan <berzan@tutanota.com>
2022-10-26 17:34:19 +0300

    add: description comment

--- sdk/src/cosmos/chains/SecretChain.ts
+++ sdk/src/cosmos/chains/SecretChain.ts
@@ -1,6 +1,10 @@
 import { Cosmos44Chain } from './Cosmos44Chain'
 import { SecretInfo } from '../chain-infos'
 
+/**
+ * # Secret
+ * The class that has methods to intereact with Secret blockchain.
+ */
 export class SecretChain extends Cosmos44Chain {
     constructor() {
         super(SecretInfo)
