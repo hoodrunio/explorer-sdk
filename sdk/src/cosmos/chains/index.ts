bfe4d736ca37d8e4b269c78276bb87dc2653b888
Berzan <berzan@tutanota.com>
2022-10-26 14:22:33 +0300

    add: new chains

--- sdk/src/cosmos/chains/index.ts
+++ sdk/src/cosmos/chains/index.ts
@@ -1,5 +1,9 @@
+import { AxelarChain } from './AxelarChain'
+import { CelestiaChain } from './CelestiaChain'
 import { EvmosChain } from './EvmosChain'
 import { KyveChain } from './KyveChain'
+import { OsmosisChain } from './OsmosisChain'
+import { SecretChain } from './SecretChain'
 
 /** Returns the wanted chain.
  *
@@ -13,6 +17,21 @@ import { KyveChain } from './KyveChain'
  * ```
  */
 export module getChain {
+    /** Returns Evmos chain that you can use to make requests to the blockchain. */
     export const Evmos = () => new EvmosChain()
+
+    /** Returns Kyve chain that you can use to make requests to the blockchain. */
     export const Kyve = () => new KyveChain()
+
+    /** Returns Axelar chain that you can use to make requests to the blockchain. */
+    export const Axelar = () => new AxelarChain()
+
+    /** Returns Osmosis chain that you can use to make requests to the blockchain. */    
+    export const Osmosis = () => new OsmosisChain()
+
+    /** Returns Celestia chain that you can use to make requests to the blockchain. */
+    export const Celestia = () => new CelestiaChain()
+    
+    /** Returns Secret chain that you can use to make requests to the blockchain. */
+    export const Secret = () => new SecretChain()
 }
