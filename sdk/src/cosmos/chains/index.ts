7ae6a29e3a3333c264d69e5f9550d459ff82fc62
Berzan <berzan@tutanota.com>
2022-10-26 15:48:14 +0300

    run: prettify

--- sdk/src/cosmos/chains/index.ts
+++ sdk/src/cosmos/chains/index.ts
@@ -26,12 +26,12 @@ export module getChain {
     /** Returns Axelar chain that you can use to make requests to the blockchain. */
     export const Axelar = () => new AxelarChain()
 
-    /** Returns Osmosis chain that you can use to make requests to the blockchain. */    
+    /** Returns Osmosis chain that you can use to make requests to the blockchain. */
     export const Osmosis = () => new OsmosisChain()
 
     /** Returns Celestia chain that you can use to make requests to the blockchain. */
     export const Celestia = () => new CelestiaChain()
-    
+
     /** Returns Secret chain that you can use to make requests to the blockchain. */
     export const Secret = () => new SecretChain()
 }
