8c7cbab4732a09a1ae774b5d285c4dd42b7d48cb
Berzan <berzan@tutanota.com>
2022-10-26 17:33:50 +0300

    add: description comment

--- sdk/src/cosmos/chains/AxelarChain.ts
+++ sdk/src/cosmos/chains/AxelarChain.ts
@@ -1,6 +1,10 @@
 import { Cosmos44Chain } from './Cosmos44Chain'
 import { AxelarInfo } from '../chain-infos'
 
+/**
+ * # Axelar
+ * The class that has methods to intereact with Axelar blockchain.
+ */
 export class AxelarChain extends Cosmos44Chain {
     constructor() {
         super(AxelarInfo)
