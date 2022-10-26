a81250e1d789662e5c8463acfd5cfa8954504ec6
Berzan <berzan@tutanota.com>
2022-10-26 17:31:20 +0300

    create: Cosmos45Chain

---
+++ sdk/src/cosmos/chains/Cosmos45Chain.ts
@@ -0,0 +1,8 @@
+import { ChainInfo } from '../types/globals'
+import { Cosmos44Chain } from './Cosmos44Chain'
+
+export class Cosmos45Chain extends Cosmos44Chain {
+    constructor(info: ChainInfo) {
+        super(info)
+    }
+}
