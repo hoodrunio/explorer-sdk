bbdd230b39290a87634fddc5bec210a39fa69b96
Berzan <berzan@tutanota.com>
2022-10-26 17:33:56 +0300

    add: description comment

--- sdk/src/cosmos/chains/CelestiaChain.ts
+++ sdk/src/cosmos/chains/CelestiaChain.ts
@@ -1,6 +1,9 @@
 import { Cosmos44Chain } from './Cosmos44Chain'
 import { CelestiaInfo } from '../chain-infos'
-
+/**
+ * # Celestia
+ * The class that has methods to intereact with Celestia blockchain.
+ */
 export class CelestiaChain extends Cosmos44Chain {
     constructor() {
         super(CelestiaInfo)
