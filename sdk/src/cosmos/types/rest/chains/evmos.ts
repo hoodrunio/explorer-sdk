70616debd43c819b7d4a6814237f91c347aecf30
Berzan <berzan@tutanota.com>
2022-10-26 13:35:16 +0300

    create: evmos.ts

---
+++ sdk/src/cosmos/types/rest/chains/evmos.ts
@@ -0,0 +1,58 @@
+import { RESTDatas } from '../cosmos-44'
+
+export type EvmosRESTMethod = keyof EvmosRESTDatas
+
+export interface EvmosRESTErrorResponse {
+    code: string
+    message: string
+    details: unknown[]
+}
+
+export type EvmosRESTSuccessResponse<T extends EvmosRESTMethod> = RESTDatas[T] extends {
+    response: infer E
+}
+    ? E
+    : never
+
+export type RESTResponse<T extends EvmosRESTMethod> =
+    | (EvmosRESTSuccessResponse<T> & { message: undefined })
+    | EvmosRESTErrorResponse
+
+export type EvmosRESTEndpoint<T extends EvmosRESTMethod> = EvmosRESTDatas[T] extends {
+    endpoint: infer E
+}
+    ? E
+    : never
+
+export type EvmosRESTPathParams<T extends EvmosRESTMethod> = EvmosRESTDatas[T] extends {
+    pathParams: infer E
+}
+    ? E
+    : never
+
+export type EvmosRESTQueryParams<T extends EvmosRESTMethod> = EvmosRESTDatas[T] extends {
+    queryParams: infer E
+}
+    ? E
+    : never
+
+type PathParam<
+    T extends EvmosRESTMethod,
+    E extends keyof EvmosRESTDatas[T]['pathParams']
+> = RESTDatas[T]['pathParams'][E]
+
+export type EvmosRESTDatas = Omit<RESTDatas, 'balance'> & {
+    /** Queries the balance of a single coin for a single account. */
+    balance: {
+        endpoint: `/cosmos/bank/v1beta1/balances/${PathParam<'balance', 'address'>}/by_denom`
+        pathParams: {
+            /** The address to query balances for. */
+            address: string
+        }
+        queryParams: {
+            /** The coin denom to query balances for. */
+            denom: string
+        }
+        response: RESTDatas['balance']
+    }
+}
