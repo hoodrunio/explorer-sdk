f927adc39bc031e544783b2a3561bad5b0373507
Berzan <berzan@tutanota.com>
2022-10-26 17:34:10 +0300

    add: description comment

--- sdk/src/cosmos/chains/KyveChain.ts
+++ sdk/src/cosmos/chains/KyveChain.ts
@@ -1,6 +1,10 @@
 import { Cosmos44Chain } from './Cosmos44Chain'
 import { KyveInfo } from '../chain-infos'
 
+/**
+ * # Kyve
+ * The class that has methods to intereact with Kyve blockchain.
+ */
 export class KyveChain extends Cosmos44Chain {
     constructor() {
         super(KyveInfo)
