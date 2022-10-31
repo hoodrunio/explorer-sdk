29bf11c88fc3dc27b7f69d0a458b38876e6581e9
Berzan <berzan@tutanota.com>
2022-10-31 12:36:31 -0700

    update: methods

--- sdk/src/chains/EvmosChain.ts
+++ sdk/src/chains/EvmosChain.ts
@@ -1,13 +1,6 @@
 import { Cosmos44Chain } from './Cosmos44Chain'
 import { EvmosInfo } from '../chain-info'
-import {
-    EvmosRESTEndpoint,
-    EvmosRESTMethod,
-    EvmosRESTPathParams,
-    EvmosRESTQueryParams,
-    EvmosRESTSuccessResponse,
-} from '../types/rest/chains/evmos'
-
+import { RESTCosmos44Params, RESTCosmos44PathParams } from '../types/rest/cosmos-44'
 /**
  * # Evmos
  * The class that has methods to intereact with Evmos blockchain.
@@ -17,26 +10,18 @@ export class EvmosChain extends Cosmos44Chain {
         super(EvmosInfo)
     }
 
-    protected restGetRequest<
-        T extends EvmosRESTMethod,
-        E = EvmosRESTEndpoint<T>,
-        Q = EvmosRESTQueryParams<T>,
-        R = EvmosRESTSuccessResponse<T>
-    >(endpoint: E, queryParams: Q) {
-        return super.restGetRequest<T, E, Q, R>(endpoint, queryParams)
-    }
 
-    async getBalance({
+    async getBalanceEvmos({
         address,
         ...query
-    }: EvmosRESTPathParams<'balance'> & EvmosRESTQueryParams<'balance'>) {
-        return this.restGetRequest<'balance'>(
+    }: RESTCosmos44PathParams<'evmosBalance'> & RESTCosmos44Params<'evmosBalance'>) {
+        return this.restGetRequest<'evmosBalance'>(
             `/cosmos/bank/v1beta1/balances/${address}/by_denom`,
             query
         )
     }
 
-    async getInflation() {
-        return this.restGetRequest<'inflation'>(`/evmos/inflation/v1/inflation_rate`, undefined)
+    async getInflationEvmos() {
+        return this.restGetRequest<'evmosInflation'>(`/evmos/inflation/v1/inflation_rate`, undefined)
     }
 }
