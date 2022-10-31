220372de3ced10147d4f91592b69e0dc9a5f6e80
Berzan <berzan@tutanota.com>
2022-10-31 14:54:06 -0700

    move: `index.ts` to `src/index.ts`

---
+++ api_generator_app/src/index.ts
@@ -0,0 +1,12 @@
+// Imports.
+import CONFIG from "../config.json"
+
+// The app starts here.
+(async () => {
+
+    console.log(CONFIG)
+
+
+
+})().catch(err => console.error(err))
+// Catch errors.
